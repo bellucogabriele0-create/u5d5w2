d750433c497bf3e3479d386799d34aa22c9196d9
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-02-12 21:32:37 +0100

    Initial commit

---
+++ src/test/java/gabrielebelluco/u5d5w2/U5d5w2ApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielebelluco.u5d5w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d5w2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
